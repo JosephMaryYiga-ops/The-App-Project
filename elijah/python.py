ef7be4c904d118d9cab0496e63660838e1be5959
Mubiru720 <mubiruelijah@gmail.com>
2026-03-02 20:24:33 +0300

    removed dot

---
+++ elijah/python.py
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int mai
+    //assingning variables 
+    int age = 25;
+    int year = 2025;
+    float CGPA = 4.45;
+    float price = 19.50;
+    float temperature = -10.054;
+
+    printf("You are %d years old\n", age);
+    printf("We in %d bro stop playing\n", year);
+    printf("Your CGPA is %.2f\n", CGPA);
+    printf("That shit costs $%.2f\n", price);
+    printf("%.3f°C! Bro dats fuckin cold\n", temperature); 
+
+    return 0;
+
+}
+   
